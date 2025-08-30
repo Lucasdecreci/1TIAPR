7c40dee4904fffbcb6b271313ac6a2733597899d
lucasdecreci <lucasdecreci@hotmail.com>
2025-08-29 23:05:23 -0300

    Add initial version of tipos.py with variable declarations and print statements

---
+++ tipos.py
@@ -0,0 +1,24 @@
+var = 1
+var2 = 10.4
+var = 15.2 
+
+#comentar o meu código
+nome = "Lucas"
+
+booleano = True
+booleano = False
+
+lista = ["macarrao", "pao", "arroz", "molho"]
+lista2 = [1,2,3,4]
+
+'''
+[ sempre usado para listar coisas]
+entre " numero vira texto
+
+'''
+
+
+resultado = var + var2
+print(resultado)
+
+print(nome)
