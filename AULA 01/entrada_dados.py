3bb53f47fcee232622c77c9090ef2472b4a68e4e
lucasdecreci <lucasdecreci@hotmail.com>
2025-09-02 22:42:26 -0300

    exercicios 02092025

---
+++ AULA 01/entrada_dados.py
@@ -0,0 +1,4 @@
+nome = input("Qual o seu nome? ")
+
+print("Olá," + nome + ", seja bem vindo!")
+print(f"Olá, {nome} !, seja bem vindo(a)!")
---
+++ AULA 01/exer1.py
@@ -0,0 +1,7 @@
+raio=5
+pi=3.14159
+
+a=pi * raio**2
+
+
+print(f"A área da circuntancia de raio {raio} é {a:.2f}")
